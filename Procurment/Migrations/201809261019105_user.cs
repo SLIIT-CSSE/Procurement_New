8c60447b87ed50ce329991347b4afb1dd73928a1
HSajinika <43310658+HSajinika@users.noreply.github.com>
2018-09-26 16:50:58 +0530

    ProcurementDb

---
+++ Procurment/Migrations/201809261019105_user.cs
@@ -0,0 +1,29 @@
+namespace Procurment.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class user : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        UserName = c.String(),
+                        Password = c.String(),
+                        Role = c.String(),
+                        Email = c.String(),
+                    })
+                .PrimaryKey(t => t.UserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
